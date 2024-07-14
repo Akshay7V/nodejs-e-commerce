24f71b33a4a9f9f26f323fdb972a115660bf251d
Akshay7V <akshayv.ramakrishnan@gmail.com>
2024-07-14 15:48:32 +0530

    created an navbar in index.hbs and created an array in index.js and passed the products to index.hbs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let products=[
+    {
+      name:"IPHONE15",
+      catrgory:"mobile",
+      discription :"this is a good phone",
+      image:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MT223?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1693248280978"
+
+    },
+    {
+      name:"SAMSUNG S22 Ultra",
+      catrgory:"mobile",
+      discription :"this is a good phone",
+      image:"https://m.media-amazon.com/images/I/41spaNtvOxL._AC_UF1000,1000_QL80_.jpg"
+
+    },
+    {
+      name:"VIVO Y200e",
+      catrgory:"mobile",
+      discription :"this is a good phone",
+      image:"https://m.media-amazon.com/images/I/61-4WCklbbL.jpg"
+
+    },
+    {
+      name:"ONE PLUS",
+      catrgory:"mobile",
+      discription :"this is a good phone",
+      image:"https://m.media-amazon.com/images/I/614cCf6bzyL._AC_UF1000,1000_QL80_.jpg"
+
+    }
+    
+  ]
+  res.render('index', { products });
 });
 
 module.exports = router;
