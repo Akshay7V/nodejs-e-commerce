6e577343d7c5f038e3ffbe86f716d3cc79376267
Akshay7V <akshayv.ramakrishnan@gmail.com>
2024-07-15 17:33:09 +0530

    created a add-product form in admin page

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,46 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index',{admin:true});
+  let products=[
+    {
+      name:"IPHONE15",
+      category:"mobile",
+      discription :"this is a good phone",
+      image:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MT223?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1693248280978"
+
+    },
+    {
+      name:"SAMSUNG S22 Ultra",
+      category:"mobile",
+      discription :"this is a good phone",
+      image:"https://m.media-amazon.com/images/I/41spaNtvOxL._AC_UF1000,1000_QL80_.jpg"
+
+    },
+    {
+      name:"VIVO Y200e",
+      category:"mobile",
+      discription :"this is a good phone",
+      image:"https://m.media-amazon.com/images/I/61-4WCklbbL.jpg"
+
+    },
+    {
+      name:"ONE PLUS",
+      category:"mobile",
+      discription :"this is a good phone",
+      image:"https://m.media-amazon.com/images/I/614cCf6bzyL._AC_UF1000,1000_QL80_.jpg"
+
+    }
+    
+  ]
+  res.render('admin/view-products',{admin:true,products});
+  
 });
+router.get('/add-product',(req,res)=>{
+  res.render('admin/add-product');
+})
+router.post('/add-product',(req,res)=>{
+  console.log(req.body);
+  console.log(req.files.Image);
+})
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -2,37 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  let products=[
+  let products = [
     {
-      name:"IPHONE15",
-      catrgory:"mobile",
-      discription :"this is a good phone",
-      image:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MT223?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1693248280978"
-
+      name: "IPHONE15",
+      category: "mobile",
+      description: "this is a good phone",
+      image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MT223?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1693248280978"
     },
     {
-      name:"SAMSUNG S22 Ultra",
-      catrgory:"mobile",
-      discription :"this is a good phone",
-      image:"https://m.media-amazon.com/images/I/41spaNtvOxL._AC_UF1000,1000_QL80_.jpg"
-
+      name: "SAMSUNG S22 Ultra",
+      category: "mobile",
+      description: "this is a good phone",
+      image: "https://m.media-amazon.com/images/I/41spaNtvOxL._AC_UF1000,1000_QL80_.jpg"
     },
     {
-      name:"VIVO Y200e",
-      catrgory:"mobile",
-      discription :"this is a good phone",
-      image:"https://m.media-amazon.com/images/I/61-4WCklbbL.jpg"
-
+      name: "VIVO Y200e",
+      category: "mobile",
+      description: "this is a good phone",
+      image: "https://m.media-amazon.com/images/I/61-4WCklbbL.jpg"
     },
     {
-      name:"ONE PLUS",
-      catrgory:"mobile",
-      discription :"this is a good phone",
-      image:"https://m.media-amazon.com/images/I/614cCf6bzyL._AC_UF1000,1000_QL80_.jpg"
-
+      name: "ONE PLUS Nord4",
+      category: "mobile",
+      description: "this is a good phone",
+      image: "https://m.media-amazon.com/images/I/614cCf6bzyL._AC_UF1000,1000_QL80_.jpg"
     }
-    
   ]
   res.render('index', { products,admin:false });
 });
